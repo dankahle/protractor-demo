0c687b2ee4f06868fd1505db0f46712ab5fa81c6
Dan Kahle <dan.kahle@gmail.com>
2018-01-20 08:36:17 -0800

    unit test AppTest

---
+++ src/app/app-test.ts
@@ -0,0 +1,71 @@
+import {ComponentFixture} from '@angular/core/testing/src/component_fixture';
+import {AppComponent} from './app.component';
+import {tick} from '@angular/core/testing';
+
+
+export class AppTest {
+  go: HTMLElement;
+  result: HTMLElement;
+  first: HTMLInputElement;
+  second: HTMLInputElement;
+  selOperator: HTMLSelectElement;
+  elem: any;
+  comp: AppComponent;
+
+  constructor(private fixture: ComponentFixture<AppComponent>) {
+    this.elem = fixture.nativeElement;
+    this.comp = fixture.componentInstance;
+    this.setProperties();
+    fixture.detectChanges();
+  }
+
+  setProperties() {
+    this.go = this.elem.querySelector('button');
+    this.result = <HTMLElement>this.elem.querySelector('#result');
+    this.first = <HTMLInputElement>this.elem.querySelector('[name="first"]');
+    this.second = <HTMLInputElement>this.elem.querySelector('[name="second"]');
+    this.selOperator = <HTMLSelectElement>this.elem.querySelector('[name="operator"]');
+  }
+
+  validate(expectedCount, expr, _result) {
+    expect(this.first.textContent).toBe('');
+    expect(this.second.textContent).toBe('');
+    expect(this.result.textContent).toBe(_result);
+
+    const history = this.elem.querySelectorAll('table tr');
+    expect(history.length).toBe(expectedCount);
+    expect(history[1].querySelector('.hist-time').textContent).toMatch(/\d{1,2}\:\d\d\:\d\d/);
+    expect(history[1].querySelector('.hist-expr').textContent).toMatch(expr);
+    expect(history[1].querySelector('.hist-result').textContent).toContain(_result);
+  }
+
+  getHistory() {
+    return this.elem.querySelectorAll('table tr');
+  }
+
+  calcComp(firstVal, secondVal, operatorVal?) {
+    this.comp.first = firstVal.toString();
+    this.comp.second = secondVal.toString();
+    if (operatorVal) {
+      this.comp.operator = operatorVal;
+    }
+    this.comp.doCalc();
+    this.fixture.detectChanges();
+  }
+
+  calcHtml(firstVal, secondVal, operatorVal?) {
+    this.first.value = firstVal.toString();
+    this.first.dispatchEvent(new Event('input'));
+    this.second.value = secondVal.toString();
+    this.second.dispatchEvent(new Event('input'));
+    if (operatorVal) {
+      // (<HTMLOptionElement>this.selOperator.querySelector(`option[value="${operatorVal}"]`)).click();
+      this.selOperator.value = operatorVal;
+      this.selOperator.dispatchEvent(new Event('change'));
+    }
+    this.go.click();
+    tick();
+    this.fixture.detectChanges();
+  }
+
+}
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -6,6 +6,7 @@ import {FormsModule} from '@angular/forms';
 import {$$} from 'protractor';
 import {By} from '@angular/platform-browser';
 import {newEvent} from '../testing/helpers';
+import {AppTest} from './app-test';
 
 
 describe('AppComponent', () => {
@@ -13,61 +14,8 @@ describe('AppComponent', () => {
   let fixture: ComponentFixture<AppComponent>;
   let elem: HTMLElement;
   let delem: DebugElement;
-  let page: Page;
+  let test: AppTest;
 
-  class Page {
-    go: HTMLElement;
-    result: HTMLElement;
-    first: HTMLInputElement;
-    second: HTMLInputElement;
-    selOperator: HTMLSelectElement;
-
-    setProperties() {
-      this.go = elem.querySelector('button');
-      this.result = <HTMLElement>elem.querySelector('#result');
-      this.first = <HTMLInputElement>elem.querySelector('[name="first"]');
-      this.second = <HTMLInputElement>elem.querySelector('[name="second"]');
-      this.selOperator = <HTMLSelectElement>elem.querySelector('[name="operator"]');
-    }
-
-    validate(expectedCount, expr, _result) {
-      expect(this.first.textContent).toBe('');
-      expect(this.second.textContent).toBe('');
-      expect(this.result.textContent).toBe(_result);
-
-      const history = elem.querySelectorAll('table tr');
-      expect(history.length).toBe(expectedCount);
-      expect(history[1].querySelector('.hist-time').textContent).toMatch(/\d{1,2}\:\d\d\:\d\d/);
-      expect(history[1].querySelector('.hist-expr').textContent).toMatch(expr);
-      expect(history[1].querySelector('.hist-result').textContent).toContain(_result);
-    }
-
-    calcComp(firstVal, secondVal, operatorVal?) {
-      comp.first = firstVal.toString();
-      comp.second = secondVal.toString();
-      if (operatorVal) {
-        comp.operator = operatorVal;
-      }
-      comp.doCalc();
-      fixture.detectChanges();
-    }
-
-    calcHtml(firstVal, secondVal, operatorVal?) {
-      this.first.value = firstVal.toString();
-      this.first.dispatchEvent(new Event('input'));
-      this.second.value = secondVal.toString();
-      this.second.dispatchEvent(new Event('input'));
-      if (operatorVal) {
-        // (<HTMLOptionElement>this.selOperator.querySelector(`option[value="${operatorVal}"]`)).click();
-        this.selOperator.value = operatorVal;
-        this.selOperator.dispatchEvent(new Event('change'));
-      }
-      this.go.click();
-      tick();
-      fixture.detectChanges();
-    }
-
-  }
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
@@ -80,58 +28,53 @@ describe('AppComponent', () => {
 
   beforeEach(async(() => {
     fixture = TestBed.createComponent(AppComponent);
-    comp = fixture.componentInstance;
-    elem = fixture.nativeElement;
-    delem = fixture.debugElement;
-    page = new Page();
-    page.setProperties();
-    fixture.detectChanges();
+    test = new AppTest(fixture);
   }));
 
 
   it('should have no history', () => {
-    expect(elem.querySelectorAll('table tr').length).toBe(1);
+    expect(test.getHistory().length).toBe(1);
   });
 
   it('should do multiple calcs - comp', () => {
-    page.calcComp(1, 2);
-    page.validate(2, /1\s*\+\s*2/, '3');
+    test.calcComp(1, 2);
+    test.validate(2, /1\s*\+\s*2/, '3');
 
-    page.calcComp(2, 3, '*');
-    page.validate(3, /2\s*\*\s*3/, '6');
+    test.calcComp(2, 3, '*');
+    test.validate(3, /2\s*\*\s*3/, '6');
 
-    page.calcComp(5, 2, '-');
-    page.validate(4, /5\s*\-\s*2/, '3');
+    test.calcComp(5, 2, '-');
+    test.validate(4, /5\s*\-\s*2/, '3');
 
-    page.calcComp(2, 5, '-');
-    page.validate(5, /2\s*\-\s*5/, '-3');
+    test.calcComp(2, 5, '-');
+    test.validate(5, /2\s*\-\s*5/, '-3');
 
-    page.calcComp(12, 2, '/');
-    page.validate(6, /12\s*\/\s*2/, '6');
+    test.calcComp(12, 2, '/');
+    test.validate(6, /12\s*\/\s*2/, '6');
 
-    page.calcComp(1, 4, '/');
-    page.validate(7, /1\s*\/\s*4/, '0.25');
+    test.calcComp(1, 4, '/');
+    test.validate(7, /1\s*\/\s*4/, '0.25');
   });
 
   it('should do multiple calcs - html', fakeAsync(() => {
 
-    page.calcHtml(1, 2);
-    page.validate(2, /1\s*\+\s*2/, '3');
+    test.calcHtml(1, 2);
+    test.validate(2, /1\s*\+\s*2/, '3');
 
-    page.calcHtml(2, 3, '*');
-    page.validate(3, /2\s*\*\s*3/, '6');
+    test.calcHtml(2, 3, '*');
+    test.validate(3, /2\s*\*\s*3/, '6');
 
-    page.calcHtml(5, 2, '-');
-    page.validate(4, /5\s*\-\s*2/, '3');
+    test.calcHtml(5, 2, '-');
+    test.validate(4, /5\s*\-\s*2/, '3');
 
-    page.calcHtml(2, 5, '-');
-    page.validate(5, /2\s*\-\s*5/, '-3');
+    test.calcHtml(2, 5, '-');
+    test.validate(5, /2\s*\-\s*5/, '-3');
 
-    page.calcHtml(12, 2, '/');
-    page.validate(6, /12\s*\/\s*2/, '6');
+    test.calcHtml(12, 2, '/');
+    test.validate(6, /12\s*\/\s*2/, '6');
 
-    page.calcHtml(1, 4, '/');
-    page.validate(7, /1\s*\/\s*4/, '0.25');
+    test.calcHtml(1, 4, '/');
+    test.validate(7, /1\s*\/\s*4/, '0.25');
   }));
 
 
